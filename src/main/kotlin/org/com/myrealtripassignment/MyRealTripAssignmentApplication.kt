30ff8c9b1fc21d0994aa997340080a3eb2989347
jaeilssss <jaeilssss@gmail.com>
2026-02-11 18:17:36 +0900

    first commit

---
+++ src/main/kotlin/org/com/myrealtripassignment/MyRealTripAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package org.com.myrealtripassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyRealTripAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyRealTripAssignmentApplication>(*args)
+}
---
+++ src/test/kotlin/org/com/myrealtripassignment/MyRealTripAssignmentApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.com.myrealtripassignment
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyRealTripAssignmentApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
